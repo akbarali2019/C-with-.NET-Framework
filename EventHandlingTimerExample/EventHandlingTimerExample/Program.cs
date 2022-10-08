fdf6efd8812260247b53607b2a971cd53807f822
Akbarali Otakhanov <otakhanov.akbarali@gmail.com>
2022-10-08 16:59:11 +0900

    Event Handling commits

---
+++ EventHandlingTimerExample/EventHandlingTimerExample/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Timers;
+
+namespace EventHandlingTimerExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Timer myTimer = new Timer(2000); // every 2 seconds
+
+            myTimer.Elapsed += MyTimer_Elapsed;
+            myTimer.Elapsed += MyTimer_Elapsed1; ;
+
+            myTimer.Start();
+
+            Console.WriteLine("Press enter to remove the red event");
+            Console.ReadLine();
+
+            myTimer.Elapsed -= MyTimer_Elapsed1;
+
+            Console.ReadLine();
+        }
+
+        private static void MyTimer_Elapsed1(object sender, ElapsedEventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Elapsed: {0:HH:mm:ss.fff}", e.SignalTime);
+        }
+
+        private static void MyTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Elapsed: {0:HH:mm:ss.fff}", e.SignalTime);
+        }
+    }
+}
