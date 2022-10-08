2c74b7067e57de3106d4e620ab18d9dacf12abf0
Akbarali Otakhanov <otakhanov.akbarali@gmail.com>
2022-10-08 16:58:36 +0900

    exceptions commits

---
+++ HandlingExceptions/HandlingExceptions/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace HandlingExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            try
+            {
+                string content = File.ReadAllText(@"C:\ Users\admin\Desktop\trainn.txt");
+                Console.WriteLine(content);
+
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("There was a problem with the filename!");
+                           
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine("There was a problem with the directoryname!");
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("There was a problem with the filename");
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Closing an application...");
+            }
+            Console.ReadLine();
+        }
+    }
+}
