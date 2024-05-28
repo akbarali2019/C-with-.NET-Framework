00d495c3506e1ad43498402f08f6d7b578d53004
Akbarali Otakhanov <otakhanov.akbarali@gmail.com>
2024-05-28 16:41:50 +0900

    update commits for Proto PFCR

---
+++ PFCR.cs
@@ -0,0 +1,35 @@
+public bool HandleRequest(int chimidForSelection, List<string> body)
+{
+    try
+    {
+        sendByChimId = chimidForSelection;
+        foreach (string data in body)
+        {
+            var key = data[..5];
+            var value = data.Substring(5, 5);
+            Console.WriteLine(key);
+            Console.WriteLine(value);
+
+            using var itemWork = factoryItem.Create();
+            var targets = itemWork.Repo.GetAll()
+                .Where(j => j.ChimIdForKey == sendByChimId && j.FullFacilityCode == key);
+            var updateRelations = itemWork.Repo.GetAll()
+                .Where(j => j.ChimIdForKey == sendByChimId && j.FullFacilityCode == value);
+
+            if (!targets.Any() || !updateRelations.Any()) return false;
+
+            var updateTarget = targets.First();
+            updateTarget.RelatedItem = updateRelations.First().Id;
+            itemWork.Repo.Update(updateTarget);
+            itemWork.Complete();
+        }
+
+        notifier.notifyItemUpdates();
+        return true;
+    }
+    catch (Exception ex)
+    {
+        _serilog.LogError($"An error occurred while handling PFRS request. Exception: {ex.Message}");
+        return false;
+    }
+}
