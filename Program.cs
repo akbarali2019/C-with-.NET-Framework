0fa340ee4b795eca0684a87a0eaea605b581408e
Akbarali Otakhanov <otakhanov.akbarali@gmail.com>
2024-08-08 14:52:22 +0900

    SlideWindow easy mode

--- Program.cs
+++ Program.cs
@@ -1,22 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace CloudFireEng
+﻿
+public class SlideWindow
 {
-    internal static class Program
+    public static void Main()
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        int[] arrayList = new int[] { 2, 1, 5, 1, 3, 2 };
+        int k = 3;
+        int max_sum = 0;
+        int window_sum = 0;
+
+        for (int i = 0; i < arrayList.Length; i++)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            window_sum += arrayList[i];
+            if (i >= k - 1)
+            {
+                max_sum = Math.Max(max_sum, window_sum);
+                window_sum -= arrayList[i - (k - 1)];
+            }
         }
+        Console.WriteLine($"max_sum: {max_sum}");
     }
 }
