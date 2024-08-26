607bb183a5eb14a75286796ab921fd56f55f4cc5
Akbarali Otakhanov <otakhanov.akbarali@gmail.com>
2024-08-26 19:13:11 +0900

    passed wat till now

--- ObjectLifetime/ObjectLifetime/Program.cs
+++ ObjectLifetime/ObjectLifetime/Program.cs
@@ -43,6 +43,7 @@ namespace ObjectLifetime
         public string Model { get; set; }
         public int Year { get; set; }
         public string Color { get; set; }
+        //public string HadDistance { get; set; }
 
 
         public Car()
